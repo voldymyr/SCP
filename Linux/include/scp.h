7bd85a5438a6d043d81c5bb645a82f7de1b62201
Voldymyr <vova.bendiuga@gmail.com>
2013-04-23 01:02:33 +0300

    Update scp.h

--- Linux/include/scp.h
+++ Linux/include/scp.h
@@ -12,6 +12,8 @@
  * You should have received a copy of the GNU Lesser General Public License along with this library;
  * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
  * 
+ * Please find full license text in License.txt
+ * 
  * Contact info: vova.bendiuga@gmail.com
  * 
  ***********************************************************************************************************/
